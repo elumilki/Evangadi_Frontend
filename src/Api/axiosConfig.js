d997363ad30283653b1acab7451bc2b3a2fc68d5
elumilki <milkiyaselili@gmail.com>
2024-06-26 03:21:56 -0400

    Evangadi forum frontend commit

--- src/Api/axiosConfig.js
+++ src/Api/axiosConfig.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 
 const axiosBase = axios.create({
-    baseURL: 'http://localhost:5500/api'
+    //baseURL: 'http://localhost:5500/api',
+    baseURL: 'https://evangadi-backend-t0yq.onrender.com/api'
 })
 
 export default axiosBase
